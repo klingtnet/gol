f0a4980d6b46f04eaf0f7b1134edc4cca0653bbc
Lucas Stadler <luc@spreadshirt.net>
2015-03-03 18:44:28 +0100

    implemented post editing
    redirecting to / after editing doesn't work yet, though.

--- main.go
+++ main.go
@@ -167,9 +167,16 @@ func main() {
 			}
 
 			var newPost Post
-			err := json.NewDecoder(r.Body).Decode(&newPost)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
+			if r.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+				newPost.Title = r.FormValue("title")
+				newPost.Content = r.FormValue("content")
+				w.Header().Set("Location", "/")
+				w.WriteHeader(http.StatusAccepted)
+			} else { // assume it's JSON
+				err := json.NewDecoder(r.Body).Decode(&newPost)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+				}
 			}
 
 			if newPost.Title != "" {
