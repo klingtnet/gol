4877660c74c773f2e0315d5d4b2a5a4ec039f857
Andreas Linz <anli@spreadshirt.net>
2015-03-03 10:16:33 +0100

    http hello world

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	fmt.Println("Listening on http://0.0.0.0:5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
