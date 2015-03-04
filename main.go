5a251eab8b10f7a894147aef5d498fed65be9902
Lucas Stadler <luc@spreadshirt.net>
2015-03-04 13:09:22 +0100

    added tooltips to edit, delete and new post buttons

--- main.go
+++ main.go
@@ -282,14 +282,14 @@ var homePageTemplateStr = `<!DOCTYPE html>
 	<body>
 		<div class="container">
 			<div id="edit-button" class="fixed-action-btn">
-				<a href="/posts/new" class="btn-floating btn-large waves-effect waves-light blue"><i class="mdi-content-add"></i></a>
+				<a href="/posts/new" class="btn-floating btn-large waves-effect waves-light blue tooltipped" data-tooltip="Write a new post"><i class="mdi-content-add"></i></a>
 			</div>
 
 			{{ range $post := .posts }}
 			<article id="post-{{ $post.Id }}" class="post">
 				<div class="post-actions">
-					<a href="/posts/{{ $post.Id }}/edit" class="btn-floating waves-effect waves-light blue"><i class="mdi-editor-mode-edit"></i></a>
-					<a href="/posts/{{ $post.Id }}" data-method="DELETE" class="btn-floating waves-effect waves-light red"><i class="mdi-action-delete"></i></a>
+					<a href="/posts/{{ $post.Id }}/edit" class="btn-floating waves-effect waves-light blue tooltipped" data-tooltip="Edit post"><i class="mdi-editor-mode-edit"></i></a>
+					<a href="/posts/{{ $post.Id }}" data-method="DELETE" class="btn-floating waves-effect waves-light red tooltipped" data-tooltip="Delete post"><i class="mdi-action-delete"></i></a>
 				</div>
 				<h1><a href="/posts/{{ $post.Id }}">{{ $post.Title }}</a></h1>
 				<h5>Posted on <i>{{ $post.Created | formatTime }}</i></h5>
