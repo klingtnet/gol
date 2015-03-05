bea84f453c82e5bb34c2e338fbc9fc39611074dc
Lucas Stadler <lu@papill0n.org>
2015-03-05 20:22:05 +0100

    improved post form markup
    point the `for` attribute to the correct id, add `required` and
    `autofocus` to the title input.

--- templates/post_form.go
+++ templates/post_form.go
@@ -7,12 +7,12 @@ var postFormTemplate = `
 
 			<form method="POST" action="/posts{{ if .post }}/{{ .post.Id }}{{ end }}">
 				<div class="input-field">
-					<input class="markdown-input" name="title" type="text" value="{{ .post.Title }}"></input>
-					<label for="title">Titlemania</label>
+					<input id="edit-title" class="markdown-input" name="title" autofocus required type="text" value="{{ .post.Title }}"></input>
+					<label for="edit-title">Titlemania</label>
 				</div>
 				<div class="input-field">
-					<textarea class="materialize-textarea markdown-input" name="content" rows="80" cols="100">{{ .post.Content }}</textarea>
-					<label for="content">Your thoughts.</label>
+					<textarea id="edit-content" class="materialize-textarea markdown-input" name="content" rows="80" cols="100">{{ .post.Content }}</textarea>
+					<label for="edit-content">Your thoughts.</label>
 				</div>
 
 
