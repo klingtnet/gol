e50ac76b146d581ba9c88fa86b60510e256d1e78
Lucas Stadler <luc@spreadshirt.net>
2015-03-06 16:11:21 +0100

    added ldap auth backend

---
+++ auth/ldap/main.go
@@ -0,0 +1,61 @@
+package ldap
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"github.com/heyLu/ldap"
+	"log"
+	"net/url"
+
+	auth ".."
+)
+
+type Backend struct{}
+
+type Auth struct {
+	addr       string
+	insecure   bool
+	dnTemplate string
+}
+
+func init() {
+	auth.Register("ldap", Backend{})
+}
+
+func (b Backend) Open(u *url.URL) (auth.Auth, error) {
+	insecure := false
+	if u.Query().Get("insecure") == "true" {
+		log.Print("Warning: insecure ldap connection!")
+		insecure = true
+	}
+
+	dnTemplate := u.Query().Get("dnTemplate")
+	if dnTemplate == "" {
+		return nil, errors.New("no dnTemplate configured")
+	}
+
+	ldapAuth := Auth{
+		addr: u.Host,
+		dnTemplate: dnTemplate,
+		insecure: insecure,
+	}
+
+	return auth.Auth(&ldapAuth), nil
+}
+
+func (a Auth) Login(username, password string) error {
+	var conn *ldap.Conn
+	var err error
+	if a.insecure {
+		tlsConfig := tls.Config{InsecureSkipVerify: true}
+		conn, err = ldap.DialSSLWithConfig("tcp", a.addr, &tlsConfig)
+	} else {
+		conn, err = ldap.DialSSL("tcp", a.addr)
+	}
+	if err != nil {
+		return err
+	}
+
+	return conn.Bind(fmt.Sprintf(a.dnTemplate, username), password)
+}
