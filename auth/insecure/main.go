2731146cb710a7d695caa367b91742c5c06a22ac
Lucas Stadler <luc@spreadshirt.net>
2015-03-06 16:22:19 +0100

    added an insecure auth backend
    it gets user/password mappings as an argument on setup.

---
+++ auth/insecure/main.go
@@ -0,0 +1,41 @@
+package insecure
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+
+	auth ".."
+)
+
+type Backend struct{}
+
+type Auth struct{
+	mapping map[string]string
+}
+
+func init() {
+	auth.Register("insecure", Backend{})
+}
+
+func (b Backend) Open(u *url.URL) (auth.Auth, error) {
+	mapping := make(map[string]string)
+	mappings := strings.Split(u.Host, ",")
+	for _, m := range mappings {
+		s := strings.Split(m, ":")
+		if len(s) != 2 {
+			return nil, errors.New("invalid user password mapping, format must be 'joe:oops,hey:there'")
+		}
+		mapping[s[0]] = s[1]
+	}
+
+	return &Auth{mapping}, nil
+}
+
+func (a Auth) Login(username, password string) error {
+	if a.mapping[username] != password {
+		return errors.New("invalid credentials")
+	}
+
+	return nil
+}
