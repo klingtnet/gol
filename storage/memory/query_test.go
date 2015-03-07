92f1953612d6246199130bf149aa1334afc7adda
Lucas Stadler <lu@papill0n.org>
2015-03-07 11:17:04 +0100

    added tests for Find on the memory backend
    still only delegating to memory, but now we know it actually works!

---
+++ storage/memory/query_test.go
@@ -0,0 +1,49 @@
+package memory
+
+import (
+	"testing"
+	"time"
+
+	"../../post"
+	storage ".."
+	tu "../../util/testing"
+)
+
+var examplePosts = []post.Post{
+	post.Post{"1", "first post", "something important!", time.Now()},
+	post.Post{"2", "second post", "a realization.", time.Now()},
+}
+
+func TestFindOnlyId(t *testing.T) {
+	store := FromPosts(examplePosts)
+
+	// find a post
+	postFindById, _ := store.FindById("1")
+	q, _ := storage.Query().Find("id", "1").Build()
+	postFind, _ := store.Find(*q)
+
+	tu.RequireEqual(t, len(postFind), 1)
+	tu.ExpectEqual(t, *postFindById, postFind[0])
+}
+
+func TestNotFindOnlyId(t *testing.T) {
+	store := FromPosts(examplePosts)
+
+	// don't find a post
+	postFindById, _ := store.FindById("3")
+	q, _ := storage.Query().Find("id", "3").Build()
+	postFind, _ := store.Find(*q)
+
+	tu.ExpectEqual(t, len(postFind), 0)
+	tu.ExpectEqual(t, postFindById, (*post.Post)(nil))
+}
+
+func TestFindAll(t *testing.T) {
+	store := FromPosts(examplePosts)
+
+	postsFindAll, _ := store.FindAll()
+	q, _ := storage.Query().Build()
+	postsFind, _ := store.Find(*q)
+
+	tu.ExpectEqual(t, len(postsFindAll), len(postsFind))
+}
