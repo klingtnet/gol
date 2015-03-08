be9fd4372e7c2fcd20996a777500e08daaeb4231
Lucas Stadler <lu@papill0n.org>
2015-03-08 09:35:37 +0100

    changed post.ByDate to sort by .UnixNano(), not .Unix()
    we can (and should) use the most precise value we have availlable.
    additionally, it would result in possibly confusing things later on.

--- post/main.go
+++ post/main.go
@@ -16,7 +16,7 @@ type ByDate []Post
 
 func (p ByDate) Len() int           { return len(p) }
 func (p ByDate) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-func (p ByDate) Less(i, j int) bool { return p[i].Created.Unix() < p[j].Created.Unix() }
+func (p ByDate) Less(i, j int) bool { return p[i].Created.UnixNano() < p[j].Created.UnixNano() }
 
 func Sort(sortable sort.Interface) sort.Interface {
 	sort.Sort(sortable)
