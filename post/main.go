429b19cf9e335617d8eaae740f752b7e3339feb4
Lucas Stadler <lu@papill0n.org>
2015-03-08 09:42:40 +0100

    added support for sorting posts by title
    (not supported by any backend yet, though.)

--- post/main.go
+++ post/main.go
@@ -18,6 +18,12 @@ func (p ByDate) Len() int           { return len(p) }
 func (p ByDate) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p ByDate) Less(i, j int) bool { return p[i].Created.UnixNano() < p[j].Created.UnixNano() }
 
+type ByTitle []Post
+
+func (p ByTitle) Len() int           { return len(p) }
+func (p ByTitle) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ByTitle) Less(i, j int) bool { return p[i].Title < p[j].Title }
+
 func Sort(sortable sort.Interface) sort.Interface {
 	sort.Sort(sortable)
 	return sortable
