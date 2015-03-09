cbaeec3d34832bf5fac6c1c98bb8ec50bec08fdf
Lucas Stadler <lu@papill0n.org>
2015-03-09 20:36:03 +0100

    implemented the multi storage

--- storage/multi/main.go
+++ storage/multi/main.go
@@ -3,6 +3,8 @@ package multi
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"net/url"
 
 	storage ".."
@@ -14,7 +16,7 @@ type Backend struct{}
 
 type Store struct {
 	primary     storage.Store
-	secondaries []storage.Store
+	secondaries map[string]storage.Store
 }
 
 func init() {
@@ -22,30 +24,76 @@ func init() {
 }
 
 func (b Backend) Open(u *url.URL) (storage.Store, error) {
-	store := storage.Store(&Store{nil, nil})
+	primaryUrl := u.Query().Get("primary")
+	if primaryUrl == "" {
+		return nil, errors.New("no primary store specified")
+	}
+	primary, err := storage.Open(primaryUrl)
+	if err != nil {
+		return nil, errors.New(fmt.Sprint("error opening primary store: ", err))
+	}
+
+	secondaryUrls := u.Query()["secondary"]
+	secondaries := make(map[string]storage.Store, len(secondaryUrls))
+	for _, secondaryUrl := range secondaryUrls {
+		secondary, err := storage.Open(secondaryUrl)
+		if err != nil {
+			errors.New(fmt.Sprintf("error opening secondary store '%s': %s", secondaryUrl, err))
+		}
+		secondaries[secondaryUrl] = secondary
+	}
+
+	store := storage.Store(&Store{primary, secondaries})
 	return store, nil
 }
 
 func (s *Store) Find(q query.Query) ([]post.Post, error) {
-	return nil, errors.New("not implemented")
+	return s.primary.Find(q)
 }
 
 func (s *Store) FindById(id string) (*post.Post, error) {
-	return nil, errors.New("not implemented")
+	return s.primary.FindById(id)
 }
 
 func (s *Store) FindAll() ([]post.Post, error) {
-	return nil, errors.New("not implemented")
+	return s.primary.FindAll()
 }
 
 func (s *Store) Create(p post.Post) error {
-	return errors.New("not implemented")
+	for u, secondary := range s.secondaries {
+		go func() {
+			err := secondary.Create(p)
+			if err != nil {
+				log.Printf("Error: [%s] create: %s", u, err)
+			}
+		}()
+	}
+
+	return s.primary.Create(p)
 }
 
 func (s *Store) Update(p post.Post) error {
-	return errors.New("not implemented")
+	for u, secondary := range s.secondaries {
+		go func() {
+			err := secondary.Update(p)
+			if err != nil {
+				log.Printf("Error: [%s] update: %s", u, err)
+			}
+		}()
+	}
+
+	return s.primary.Update(p)
 }
 
 func (s *Store) Delete(id string) error {
-	return errors.New("not implemented")
+	for u, secondary := range s.secondaries {
+		go func() {
+			err := secondary.Delete(id)
+			if err != nil {
+				log.Printf("Error: [%s] delete: %s", u, err)
+			}
+		}()
+	}
+
+	return s.primary.Delete(id)
 }
