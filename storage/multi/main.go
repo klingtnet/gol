c89d375fc56f063f7098d59a3c290e5d4905f30b
Lucas Stadler <lu@papill0n.org>
2015-03-09 20:55:06 +0100

    fixed writing to multiple storages
    same problem as in javascript, the one loop variable is written to
    multiple times and only the last value is used.

--- storage/multi/main.go
+++ storage/multi/main.go
@@ -60,39 +60,39 @@ func (s *Store) FindAll() ([]post.Post, error) {
 }
 
 func (s *Store) Create(p post.Post) error {
-	for u, secondary := range s.secondaries {
-		go func() {
+	for u, s := range s.secondaries {
+		go func(secondary storage.Store) {
 			err := secondary.Create(p)
 			if err != nil {
 				log.Printf("Error: [%s] create: %s", u, err)
 			}
-		}()
+		}(s)
 	}
 
 	return s.primary.Create(p)
 }
 
 func (s *Store) Update(p post.Post) error {
-	for u, secondary := range s.secondaries {
-		go func() {
+	for u, s := range s.secondaries {
+		go func(secondary storage.Store) {
 			err := secondary.Update(p)
 			if err != nil {
 				log.Printf("Error: [%s] update: %s", u, err)
 			}
-		}()
+		}(s)
 	}
 
 	return s.primary.Update(p)
 }
 
 func (s *Store) Delete(id string) error {
-	for u, secondary := range s.secondaries {
-		go func() {
+	for u, s := range s.secondaries {
+		go func(secondary storage.Store) {
 			err := secondary.Delete(id)
 			if err != nil {
 				log.Printf("Error: [%s] delete: %s", u, err)
 			}
-		}()
+		}(s)
 	}
 
 	return s.primary.Delete(id)
