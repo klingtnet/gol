bd6c740650c1006254474bc46f4f8123cdf80d38
Lucas Stadler <luc@spreadshirt.net>
2015-03-10 14:56:53 +0100

    made sassc build faster
    everyone has four cores, right?

--- build_sassc.sh
+++ build_sassc.sh
@@ -11,7 +11,7 @@ if [ $? -eq 0 ]; then
     curl -L "https://github.com/sass/libsass/archive/master.tar.gz" | tar -xvz -C "$TEMPDIR"
     curl -L "https://github.com/sass/sassc/archive/master.tar.gz" | tar -xvz -C "$TEMPDIR"
     pushd $TEMPDIR/sassc-master
-    SASS_LIBSASS_PATH="$TEMPDIR/libsass-master" make build-static
+    SASS_LIBSASS_PATH="$TEMPDIR/libsass-master" make -j4 build-static
     popd
     cp --verbose "$TEMPDIR/sassc-master/bin/sassc" "$DEST/sassc"
 else
