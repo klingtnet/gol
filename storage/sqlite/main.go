19e9da4babde048b35acd77ef519f47cb4990abb
Andreas Linz <klingt.net@gmail.com>
2015-09-25 18:49:25 +0200

    Ugly Find() implementation (will be refactored)
    - `(Date)Range`, `Reverse` and matching on multiple fields `Matches` is not supported
      at the moment
    - refactor to SQL query builder

--- storage/sqlite/main.go
+++ storage/sqlite/main.go
@@ -3,7 +3,7 @@ package sqlite
 import (
 	"database/sql"
 	"errors"
-	//	"fmt"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/url"
@@ -53,7 +53,44 @@ func (m Backend) Open(u *url.URL) (storage.Store, error) {
 }
 
 func (s *Store) Find(q query.Query) ([]post.Post, error) {
-	return nil, errors.New("queries not supported")
+	posts := make([]post.Post, 0)
+	var query string
+	var err error = nil
+	var rows *sql.Rows
+	if q.Find != nil {
+		query = fmt.Sprintf("SELECT * FROM posts WHERE %s = ? ORDER BY ? DESC", q.Find.Name)
+		stmt, err := s.db.Prepare(query)
+		if err != nil {
+			return nil, err
+		}
+
+		rows, err = stmt.Query(q.Find.Value, q.SortBy)
+	} else {
+		query = "SELECT * FROM posts ORDER BY ? DESC"
+		stmt, err := s.db.Prepare(query)
+		if err != nil {
+			return nil, err
+		}
+
+		rows, err = stmt.Query(q.SortBy)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var title, content, id string
+		var created time.Time
+		err = rows.Scan(&id, &created, &title, &content)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post.Post{id, title, content, created})
+	}
+
+	return posts, nil
 }
 
 // Store interface methods
