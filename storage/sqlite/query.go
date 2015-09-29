8d6fd41b6228831f35b357f275c3c88576ce4a07
Andreas Linz <klingt.net@gmail.com>
2015-09-29 14:20:58 +0200

    Implement SQL clause builder
    - I should use github.com/Masterminds/squirrel in the near future

--- storage/sqlite/query.go
+++ storage/sqlite/query.go
@@ -1,42 +1,109 @@
 package sqlite
 
 import (
-	//	"errors"
+	"bytes"
 	"fmt"
+	"strings"
+	"text/template"
 	"time"
 
 	"../../post"
 	"../query"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	//  "github.com/Masterminds/squirrel" // use this in the future
 )
 
-func (s *Store) Find(q query.Query) ([]post.Post, error) {
-	posts := make([]post.Post, 0)
-	var query string
-	var err error = nil
-	var rows *sql.Rows
+func buildClause(a, b interface{}, op string) string {
+	return fmt.Sprintf("%s %s \"%s\"", a, op, b)
+}
+
+type SqlQuery struct {
+	Select string
+	From   string
+	Where  string
+	Order  string
+	SortBy string
+}
+
+const sqlTemplate = `SELECT {{ .Select }} FROM {{ .From }}
+WHERE {{ .Where }}
+ORDER BY {{ .SortBy }} {{ .Order }};`
+
+func buildSqlQuery(q query.Query) (string, error) {
+	sqlQuery := SqlQuery{
+		Select: "*",
+		From:   "posts"}
+
+	var whereClauses []string
 	if q.Find != nil {
-		query = fmt.Sprintf("SELECT * FROM posts WHERE %s = ? ORDER BY ? DESC", q.Find.Name)
-		stmt, err := s.db.Prepare(query)
-		if err != nil {
-			return nil, err
-		}
-
-		rows, err = stmt.Query(q.Find.Value, q.SortBy)
-	} else {
-		query = "SELECT * FROM posts ORDER BY ? DESC"
-		stmt, err := s.db.Prepare(query)
-		if err != nil {
-			return nil, err
-		}
-
-		rows, err = stmt.Query(q.SortBy)
+		whereClauses = append(whereClauses,
+			buildClause(q.Find.Name, q.Find.Value, "="))
 	}
 
+	if q.Matches != nil && len(q.Matches) > 0 {
+		for _, field := range q.Matches {
+			whereClauses = append(whereClauses,
+				buildClause(field.Name, field.Value, "="))
+		}
+	}
+
+	if q.RangeStart != nil {
+		whereClauses = append(whereClauses,
+			buildClause("created", q.RangeStart, "<"))
+	}
+	if q.RangeEnd != nil {
+		whereClauses = append(whereClauses,
+			buildClause("created", q.RangeStart, "<"))
+	}
+	sqlQuery.Where = strings.Join(whereClauses, "\nAND ")
+	if sqlQuery.Where == "" {
+		sqlQuery.Where = "TRUE"
+	}
+
+	sqlQuery.Order = "ASC"
+	if q.Reverse {
+		sqlQuery.Order = "DESC"
+	}
+
+	sqlQuery.SortBy = "created"
+	if q.SortBy != "" {
+		sqlQuery.SortBy = q.SortBy
+	}
+
+	tmpl, err := template.New("sqlQuery").Parse(sqlTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	var query bytes.Buffer
+	err = tmpl.Execute(&query, sqlQuery)
+	if err != nil {
+		return "", err
+	}
+
+	return query.String(), nil
+}
+
+func (s *Store) Find(q query.Query) ([]post.Post, error) {
+	var posts []post.Post
+	var rows *sql.Rows
+
+	query, err := buildSqlQuery(q)
 	if err != nil {
 		return nil, err
 	}
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err = stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+
 	defer rows.Close()
 
 	for rows.Next() {
